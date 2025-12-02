rjarry/libecoli

commit 3e855c5ba852279d708d28a55d85d6c372ec7bf0

src: fix scan-build errors
Signed-off-by: Robin Jarry <robin@jarry.cc>

examples/readline/main.c
@@ -143,6 +143,7 @@ static int show_help(int ignore, int invoking_key)
 	size_t count;
 	char **helps = NULL;
 	int match = 0;
+	int ret = 1;
 	int cols;
 
 	(void)ignore;
@@ -201,7 +202,7 @@ static int show_help(int ignore, int invoking_key)
 	rl_display_match_list(helps, count + match, cols);
 	rl_forced_update_display();
 
-	return 0;
+	ret = 0;
 
 fail:
 	ec_free(item);
@@ -214,7 +215,7 @@ fail:
 	}
 	free(helps);
 
-	return 1;
+	return ret;
 }
 
 static int create_commands(void)

src/ecoli_complete.c
@@ -254,6 +254,9 @@ ec_comp_item(enum ec_comp_type type,
 		if (start_cp == NULL)
 			goto fail;
 
+		if (full == NULL)
+			goto fail;
+
 		if (ec_str_startswith(full, start)) {
 			comp_cp = ec_strdup(&full[strlen(start)]);
 			if (comp_cp == NULL)

src/ecoli_editline.c
@@ -435,8 +435,9 @@ static int get_node_help(const struct ec_comp_item *item,
 
 fail:
 	ec_free(node_desc);
-	ec_free(help->desc);
 	ec_free(help->help);
+	help->desc = NULL;
+	help->help = NULL;
 	return -1;
 }
 

src/ecoli_log.c
@@ -205,7 +205,7 @@ static int ec_log_testcase(void)
 	testres |= EC_TEST_CHECK(ret != 0,
 				"should not be able to set log level\n");
 
-	ret = ec_log_fct_register(NULL, NULL);
+	ec_log_fct_register(NULL, NULL);
 	ec_log_level_set(LOG_DEBUG);
 	EC_LOG(LOG_DEBUG, "test log\n");
 	ec_log_level_set(LOG_INFO);

src/ecoli_malloc.c
@@ -170,6 +170,8 @@ static int ec_malloc_testcase(void)
 	 * time */
 	ptr = ec_calloc(3, atoll("9223372036854775807")); /* (1 << 63) - 1 */
 	testres |= EC_TEST_CHECK(ptr == NULL, "bad overflow check in ec_calloc\n");
+	ec_free(ptr);
+	ptr = NULL;
 
 	return testres;
 }

src/ecoli_node.c
@@ -597,8 +597,7 @@ static int ec_node_testcase(void)
 
 	/* same loop test, but keep some refs (released later) */
 	expr = ec_node("or", EC_NO_ID);
-	ec_node_clone(expr);
-	expr2 = expr;
+	expr2 = ec_node_clone(expr);
 	val = ec_node_int(EC_NO_ID, 0, 10, 0);
 	op = ec_node_str(EC_NO_ID, "!");
 	seq = EC_NODE_SEQ(EC_NO_ID,

src/ecoli_node_cmd.c
@@ -496,8 +496,10 @@ static int ec_node_cmd_set_config(struct ec_node *node,
 	return 0;
 
 fail:
-	for (i = 0; i < len; i++)
-		ec_node_free(table[i]);
+	if (table != NULL) {
+		for (i = 0; i < len; i++)
+			ec_node_free(table[i]);
+	}
 	ec_free(table);
 	ec_free(cmd_str);
 	ec_node_free(cmd);

src/ecoli_node_or.c
@@ -111,7 +111,7 @@ static int ec_node_or_set_config(struct ec_node *node,
 	table = ec_node_config_node_list_to_table(
 		ec_config_dict_get(config, "children"), &len);
 	if (table == NULL)
-		goto fail;
+		return -1;
 
 	for (i = 0; i < priv->len; i++)
 		ec_node_free(priv->table[i]);
@@ -120,12 +120,6 @@ static int ec_node_or_set_config(struct ec_node *node,
 	priv->len = len;
 
 	return 0;
-
-fail:
-	for (i = 0; i < len; i++)
-		ec_node_free(table[i]);
-	ec_free(table);
-	return -1;
 }
 
 static size_t

commit 128137e3e9a020543f541d8d10fc950a886469d5

examples/readline: fix double free
Fix the double free introduced by the referenced commit.

Fixes: 3e855c5ba852 ("src: fix scan-build errors")
Signed-off-by: Olivier Matz <zer0@droids-corp.org>
Reviewed-by: Robin Jarry <robin@jarry.cc>

examples/readline/main.c
@@ -177,8 +177,6 @@ static int show_help(int ignore, int invoking_key)
 		count++;
 	}
 
-	free(item);
-	ec_comp_free(c);
 	/* ensure not more than 1 entry per line */
 	rl_get_screen_size(NULL, &cols);
 	rl_display_match_list(helps, count + match, cols);