rjarry/libecoli

commit 58475824b98722246379d8cca5191838ffcfdbaf

string: introduce a function to quote a string
It can for instance be used to print the representation of a string.
This function will be used in the next commit.

Signed-off-by: Olivier Matz <zer0@droids-corp.org>
Reviewed-by: Robin Jarry <robin@jarry.cc>

src/string.c
@@ -108,3 +108,57 @@ int ec_str_parse_ullint(const char *str, unsigned int base, uint64_t min,
 	errno = save_errno;
 	return 0;
 }
+
+char *ec_str_quote(const char *str, char quote)
+{
+	size_t len = 3; /* start/end quotes + \0 */
+	const char *s;
+	char *out, *o;
+	char c;
+
+	if (quote == 0) {
+		if (strchr(str, '"') == NULL)
+			quote = '"';
+		else
+			quote = '\'';
+	}
+
+	for (s = str, c = *s; c != '\0'; s++, c = *s) {
+		if (c == quote || c == '\\')
+			len += 2;
+		else if (!isprint(c) && c != '\n')
+			len += 4;
+		else
+			len += 1;
+	}
+
+	out = malloc(len);
+	if (out == NULL)
+		return NULL;
+
+	o = out;
+	*o++ = quote;
+
+	for (s = str, c = *s; c != '\0'; s++, c = *s) {
+		if (c == quote) {
+			*o++ = '\\';
+			*o++ = quote;
+		} else if (c == '\\') {
+			*o++ = '\\';
+			*o++ = '\\';
+		} else if (!isprint(c) && c != '\n') {
+			char buf[5];
+
+			snprintf(buf, sizeof(buf), "\\x%2.2x", c);
+			memcpy(o, buf, 4);
+			o += 4;
+		} else {
+			*o++ = c;
+		}
+	}
+
+	*o++ = quote;
+	*o = '\0';
+
+	return out;
+}

include/ecoli/string.h
@@ -62,4 +62,18 @@ int ec_str_parse_llint(const char *str, unsigned int base, int64_t min,
  */
 int ec_str_parse_ullint(const char *str, unsigned int base, uint64_t min,
 			uint64_t max, uint64_t *val);
+
+/**
+ * Quote a string, escaping nested quotes.
+ *
+ * @param str
+ *   The string to quote.
+ * @param quote
+ *   The quote character to use: usually " or ' but can be anything. If 0,
+ *   select between " or ' automatically.
+ * @return
+ *   An allocated string, that must be freed by the caller using free().
+ */
+char *ec_str_quote(const char *str, char quote);
+
 /** @} */

commit 58e6b6f8b17317165a62f3955fd0a3d42c3ea0f4

string: fix signed char overflow in hex escape formatting
When c holds a byte with the high bit set (0x80-0xFF), it is
sign-extended to int before being passed to snprintf. The %2.2x format
with a negative int value produces more than 2 hex digits (e.g.
\xffffff80 for 0x80), overflowing the buf[5] array.

Cast to unsigned char to prevent sign extension.

Fixes: 58475824b987 ("string: introduce a function to quote a string")
Signed-off-by: Robin Jarry <rjarry@redhat.com>

src/string.c
@@ -161,7 +161,7 @@ char *ec_str_quote(const char *str, char quote, bool force)
 		} else if (!isprint(c) && c != '\n') {
 			char buf[5];
 
-			snprintf(buf, sizeof(buf), "\\x%2.2x", c);
+			snprintf(buf, sizeof(buf), "\\x%2.2x", (unsigned char)c);
 			memcpy(o, buf, 4);
 			o += 4;
 		} else {